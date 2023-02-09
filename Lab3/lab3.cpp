fa3c1d0a20a62c4cab5c71bcd2dbcdcc1aa1d4f1
sethemit <sethemit@icloud.com>
2023-02-08 19:43:41 -0500

    initial commit

--- Lab3/lab3.cpp
+++ Lab3/lab3.cpp
@@ -1,1 +1,24 @@
-//
+/*
+Course:  CS215-012
+Project: Lab #3
+Purpose: 
+
+Author:  Seth Hammonds
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int userInput = 0;
+
+    cout << "Please enter the 8-digit credit card number (enter -1 to quit): ";
+    cin >> userInput;
+    while (userInput != -1) {
+    
+    //userInput = -1
+    }
+    cout << "Thank you for using 'Credit Card Number Validation'!" << endl;
+    return 0;
+}
